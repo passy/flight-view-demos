4407179fad8781fdbd6bdc6b007e9ecf57335d33
Pascal Hartig <passy@twitter.com>
2013-10-18 12:15:54 +0100

    Initial setup for flight-ember

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flight View Demos
+
+Various (that's the plan, at least) experiments of combining FlightJS with View
+components of other frameworks.
---
+++ flight-ember/js/page/default.js
@@ -0,0 +1,24 @@
+define(function (require) {
+
+  'use strict';
+
+  /**
+   * Module dependencies
+   */
+
+  // var searchColumn = require('component/search_column');
+
+  /**
+   * Module exports
+   */
+
+  return initialize;
+
+  /**
+   * Module function
+   */
+
+  function initialize() {
+    console.log('Initialized.');
+  }
+});
